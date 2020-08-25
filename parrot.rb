a4b53173b7b861af6e6f7776bc4f92f4304dd5e6
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-25 03:11:18 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase)
-  puts "#{phrase}!"
+def parrot
+  puts "Pretty Birdy"
   return "Squawk!"
-  "#{phrase}"
+  "Pretty Birdy"
 end
 
